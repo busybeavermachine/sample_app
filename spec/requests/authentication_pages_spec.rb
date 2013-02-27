08b4346dfafc2eb4c2b50cf7f5e2cbe694487923
Manuel Huber <huberma@gmail.com>
2013-02-27 04:37:13 +0100

    Ex chap 9

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -50,17 +50,37 @@ describe "Authentication" do
 		describe "for non-signed-in users" do
 			let (:user) { FactoryGirl.create(:user) }
 
+			describe "visiting home page" do
+
+				before { visit root_path }
+
+				it { should_not have_link('Users') }
+				it { should_not have_link('Profile') }
+				it { should_not have_link('Setting') }
+				it { should_not have_link('Sign out') }			
+			end
+
 			describe "when attempting to visit a protected page" do
 				before do
 					visit edit_user_path(user)
-					fill_in "Email",    with: user.email
-					fill_in "Password", with: user.password
-					click_button "Sign in"
+					sign_in(user)
 				end
 
 				describe "after signing in" do
 					it "should render the desired protected page" do
 						page.should have_selector('title', text: 'Edit user')
+					end
+				end
+
+				describe "when signing in again" do
+					before do
+						delete signout_path
+						sign_in (user)
+					end
+
+					it "should render the default (profile) page" do
+						page.should_not have_selector('title', text: 'Edit user')
+						page.should have_selector('title', text: user.name)						
 					end
 				end
 			end
